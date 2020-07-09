7a13e30d4bdcd21b8852e70388b1b8528de57baa
milovetingting <milovetingting@qq.com>
2020-07-09 11:19:37 +0800

    优化布局

--- app/src/main/java/com/wangyz/wanandroid/kotlin/adapter/BindingAdapter.kt
+++ app/src/main/java/com/wangyz/wanandroid/kotlin/adapter/BindingAdapter.kt
@@ -1,14 +1,17 @@
 package com.wangyz.wanandroid.kotlin.adapter
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.wangyz.wanandroid.kotlin.R
 
 
 class BindingAdapter {
 
     companion object {
+
         @JvmStatic
         @BindingAdapter("imageUrl")
         fun bindImageUrl(view: ImageView, imageUrl: String) {
@@ -20,6 +23,24 @@ class BindingAdapter {
                 .load(imageUrl)
                 .apply(options)
                 .into(view)
+        }
+
+        @JvmStatic
+        @BindingAdapter("icon")
+        fun bindIcon(view: TextView, hasAuthor: Boolean) {
+            if (hasAuthor) {
+                val drawableLeft = view.context.resources.getDrawable(
+                    R.drawable.author
+                )
+                view.setCompoundDrawablesWithIntrinsicBounds(drawableLeft, null, null, null)
+                view.compoundDrawablePadding = 10
+            } else {
+                val drawableLeft = view.context.resources.getDrawable(
+                    R.drawable.share
+                )
+                view.setCompoundDrawablesWithIntrinsicBounds(drawableLeft, null, null, null)
+                view.compoundDrawablePadding = 10
+            }
         }
     }
 
